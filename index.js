394746da6203ffe65b0bab67acf9651274ba7769
Joel Garcia Jr <joel.garciajr84@gmail.com>
2017-02-16 21:12:22 -0200

    Starting some echos

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 var token = "EAAQ3GcyyjAwBAMVXSYtrI5ysZA3MIxYxRMxtzTT77TiwuZCdOpCtqSULUMCJsqndWOGbxQKZCtrOTmQ6VXd7Rk92uzG45Qh7mjx96on33eBjcx8nNIn4u9ZCxqVAvCglICoAljsZCdi51lgZBjIzJZBh5r7GUlzn1QCB7BhQXjT9wZDZD";
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'EAAQ3GcyyjAwBAMVXSYtrI5ysZA3MIxYxRMxtzTT77TiwuZCdOpCtqSULUMCJsqndWOGbxQKZCtrOTmQ6VXd7Rk92uzG45Qh7mjx96on33eBjcx8nNIn4u9ZCxqVAvCglICoAljsZCdi51lgZBjIzJZBh5r7GUlzn1QCB7BhQXjT9wZDZD') {
+    if (req.query['hub.verify_token'] === 'AIRBUSA320!') {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong token');
